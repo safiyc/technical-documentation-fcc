608a17ad932528094d879916311bc65498ceae77
Safiy Cham <csafiy@yahoo.com>
2019-05-02 08:41:47 -0700

    Adjusted layout of site to pass project requirements

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,21 @@ let toggleCollapse = document.getElementsByClassName("headlines");
 
 let collapseClass = document.getElementsByClassName("collapse_class");
 
-// if (screen.width <= 768) {
-//   console.log("small screen")
-// }
+window.addEventListener('resize', isResized);
 
-window.addEventListener('resize', resize);
+function isResized() {
 
-function resize() {
   if (window.innerWidth > 768) {
     for (let i = 0; i < toggleCollapse.length; i++) {
       toggleCollapse[i].removeAttribute("data-toggle");
-      collapseClass[i].setAttribute("class", "show");
+      collapseClass[i].classList.add("show");
     }
   } else {
     for (let i = 0; i < toggleCollapse.length; i++) {
       toggleCollapse[i].setAttribute("data-toggle", "collapse");
-      collapseClass[i].removeAttribute("class", "show");
+      collapseClass[i].classList.remove("show");
     }
   }
 }
+
+window.onload = isResized;
